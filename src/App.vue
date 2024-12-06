b5c46abeaef1d740332106e4e1ff1057a68010f7
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-12-06 10:26:44 +0100

    feat: add banner images and description

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue'
+import Banner from './components/Banner.vue'
 export default{
   components:{
-    Header
+    Header,
+    Banner,
   }
 }
 
@@ -12,12 +14,25 @@ export default{
 <template>
   <Header/>
   <main>
-    Main
+
+    <section>
+      <Banner/>
+    </section>
+    
   </main>
 </template>
 
 <style lang="scss">
 @use './components/style/general' as *;
 @use './components/style/variables' as *;
+
+//proprietà body
+
+p,
+.title,
+.product-name {
+    font-family: $h-font;
+    font-weight: $t-weight;
+}
 
 </style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+    name: 'Banner'
+}
+</script>
+
+<template>
+
+   <div class="row g-0">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <div class="banner" id="image-duck">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <div class="banner" id="image-base">
+                <div class="text-box">
+                    <h1 class="title">Lorem ipsum sit amet</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut asperiores eveniet, laborum
+                        omnis
+                        cupiditate
+                        blanditiis
+                        iste accusantium quam repudiandae dolorum beatae ipsa
+                    </p>
+                    <button>Acquista subito</button>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+   
+   
+</template>
+
+<style lang="scss" scoped>
+
+.banner{
+    min-height: 300px;
+    background-size:cover;
+    background-position: center;
+}
+
+
+#image-duck{
+    background-image: url('../../public/img/banner-image.png');
+}
+
+#image-base {
+    background-image: url('../../public/img/banner-bg.png');
+    
+}
+
+.text-box{
+    padding: 1.5rem;
+}
+
+    
+</style>
