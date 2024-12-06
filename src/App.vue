60e1652f85d27aa7c47180de53f4f2d79fd2c876
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-12-06 09:53:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+export default{
+  components:{
+    Header
+  }
+}
+
+</script>
+
+<template>
+  <Header/>
+  <main>
+    Main
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@use './components/style/general' as *;
+@use './components/style/variables' as *;
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name:'Header'
+}
+</script>
+
+<template>
+    Header
+</template>
+
+<style lang="scss">
+    
+</style>
